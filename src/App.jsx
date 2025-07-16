bb54c8dd1b6cfe9157d62c0a001c91bbd1cc844e
Tam Le <lengoctam0106@gmail.com>
2025-07-16 19:08:30 -0400

    search+ hero page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState, useEffect } from "react";
-
-const Card = ({title}) => {
-    const [count, setCount] = useState(0);
-    const [hasLiked, setHasLiked] = useState(false);
-
-    useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`);
-    }, [hasLiked]);
-
-    return (
-        <div className="card" onClick={() => setCount( count + 1)}>
-            <h2>{title}<br/>{count}</h2>
-
-            <button onClick={() => setHasLiked(!hasLiked)}>
-                {hasLiked ? 'Liked' : 'Like'}
-            </button>
-        </div>
-    )
-}
+import Search from './components/Search.jsx'
+import { useState } from 'react'
 
 const App = () => {
+const [searchTerm, setSearchTerm] = useState('')
 
-    return (
-        <div className="card-container">
-            <Card title="Star Wars" rating={5} isCool={true} />
-            <Card title="Avatar"/>
-            <Card title="Lion King"/>
+  return (
+    <main>
+        <div className="pattern"/>
+
+        <div className='wrapper'>
+            <header>
+                <img src="./hero.png" alt="Hero Banner" />
+                <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+            </header>
+
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            <h1 className='text-white'>{searchTerm}</h1>
         </div>
-    )
+    </main>
+  )
 }
 
 export default App
